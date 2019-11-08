78107d52a857a771a059c2cac80623bc36553043
mmandapati <mmandapati@INHYDLMMANDAPAT>
2019-11-08 10:07:37 +0530

    adding new sln

---
+++ Extensions/String.cs
@@ -0,0 +1,16 @@
+namespace CSharp.Extensions {
+    public static class String {
+        /// <summary>
+        /// Checks string object's value to array of string values
+        /// </summary>        
+        /// <param name="stringValues">Array of string values to compare</param>
+        /// <returns>Return true if any string value matches</returns>
+        public static bool In(this string value, params string[] stringValues) {
+            foreach (string otherValue in stringValues)
+                if (string.Compare(value, otherValue) == 0)
+                    return true;
+
+            return false;
+        }
+    }
+}
