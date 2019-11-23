4967c88eea85c5a7fc18bf5e520ae755aa138d08
mmandapati <madhavavarma.m@gmail.com>
2019-11-23 20:42:14 +0530

    program and startup

---
+++ WebApi/Startup.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CSharp.WebApi {
+    public class Startup {
+
+        public void ConfigureServices(IServiceCollection services) {
+
+        }
+
+        public void Configure(IApplicationBuilder application, IWebHostEnvironment env) {
+            
+        }
+    }
+}
